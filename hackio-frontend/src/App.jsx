6108c134f0de2df4b4d0b6e8d82fb8e080a9f920
franb0 <128163570+franb0@users.noreply.github.com>
2023-08-23 12:39:29 -0300

    Add files via upload

--- hackio-frontend/src/App.jsx
+++ hackio-frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/header';
-import Cart from "./components/cart/cart"
+import Cart from "./components/cart/cart";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Cart></Cart>
+      <Router>
+        <Switch>
+          <Route path='/'>
+            <Header />
+            <Cart />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- hackio-frontend/src/index.js
+++ hackio-frontend/src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    {/* <Router>
       <Routes>
-        <Route path='/' element={<App />}/>
+        <Route path='/home' element={<App />}/>
       </Routes>
-    </Router>
+    </Router> */}
   </React.StrictMode>
 );
 
